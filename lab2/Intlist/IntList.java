f5a9b4ad325ca6956a0a30fc5fceb983448560bb
Edwin Hung <32249535+edwinhung@users.noreply.github.com>
2024-01-21 15:51:58 -0800

    lab1

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,12 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A.rest == null) {
+            A.rest = B;
+            return A;
+        }
+        dcatenate(A.rest, B);
+        return A;
     }
 
     /**
@@ -91,7 +96,12 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A == null && B.rest == null) {
+            return new IntList(B.first, null);
+        } else if (A == null && B.rest != null) {
+            return new IntList(B.first, catenate(A, B.rest));
+        }
+        return new IntList(A.first, catenate(A.rest, B));
     }
 
 
